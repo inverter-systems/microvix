f0b5dbc3f541c2058b0f4a7a8e32dab5c61a2dde
Cristiano Rosa <cristiano@inverter.com.br>
2022-04-19 12:18:31 -0400

    fix: adjust config spring

---
+++ src/main/java/com/topinternacional/linx/config/SecurityWebConfig.java
@@ -0,0 +1,42 @@
+package com.topinternacional.linx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/static/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/login").permitAll()
+			.and()
+			.logout().permitAll()
+			.and().httpBasic();
+	}
+	
+   	@Override
+   	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+   		auth.inMemoryAuthentication()
+   			.passwordEncoder(passwordEncoder())
+   			.withUser("inv")
+   			.password(passwordEncoder().encode("inv"))
+   			.roles("USER")
+   			.and()
+   			.withUser("admin")
+   			.password(passwordEncoder().encode("TopSuporte"))
+   			.roles("USER");
+   }
+
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+       return new BCryptPasswordEncoder();
+   } 
+}
---
+++ src/main/java/com/topinternacional/linx/config/SpringWebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.topinternacional.linx.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+}
